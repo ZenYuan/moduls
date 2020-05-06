b4f8783433d044e7d8f0c51618508b706fb617c0
zenyuan <zhangyuanyuan22@hikvision.com>
2020-05-06 11:57:05 +0800

    修改cmakeList，设置本地编译器为8.3.0

--- AsyLog/src/LogTest.cc
+++ AsyLog/src/LogTest.cc
@@ -1,19 +1,9 @@
 
 #include <sys/types.h>
-
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
 #include <sys/stat.h>
-#ifndef WIN32
 #include <sys/queue.h>
 #include <unistd.h>
-#endif
 #include <time.h>
-#ifdef HAVE_SYS_TIME_H
-#include <sys/time.h>
-#endif
 
 #include <fcntl.h>
 #include <stdlib.h>
@@ -22,32 +12,11 @@
 #include <errno.h>
 
 
-#include <event.h>
-#include <evutil.h>
-
 #include "Logging.h"
 #include "Logging.cc"
 
 #include <pthread.h>
 static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
-
-int lasttime;
-
-static void
-timeout_cb(int fd, short event, void *arg)
-{
-	struct timeval tv;
-	struct event *exit_timeout_cb = static_cast<struct event*>(arg);
-	int newtime = time(NULL);
-
-	// LOG_ERROR(" :called at %d: %d\n", newtime, newtime - lasttime);
-	lasttime = newtime;
-
-	evutil_timerclear(&tv);
-	tv.tv_sec = 2;
-	event_add(exit_timeout_cb, &tv);
-	sleep(2);
-}
 
 
 class Global_obj
@@ -78,22 +47,6 @@ Global_obj A;
 int
 main (int argc, char **argv)
 {
-	// struct event timeout;
-	// struct timeval tv;
-
-	// /* Initalize the event library */
-	// event_init();
-
-	// /* Initalize one event */
-	// evtimer_set(&timeout, timeout_cb, &timeout);
-
-	// evutil_timerclear(&tv);
-	// tv.tv_sec = 2;
-	// event_add(&timeout, &tv);
-	// lasttime = time(NULL);
-
-	// event_dispatch();
-
 	pthread_t thr;
 	int s = pthread_create(&thr, NULL, threadFunc, NULL);
 	if(s != 0)
@@ -101,7 +54,7 @@ main (int argc, char **argv)
 		LOG_ERROR("create thread error!");
 		exit(1);
 	}
-	sleep(30);
+	sleep(4);
 
 
 	return (0);
